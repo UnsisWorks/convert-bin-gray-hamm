5d7ad5b6c66ee08409d0d83f9da362ce797f3fb4
jared0192galicia <jared0192galicia@gmail.com>
2022-10-19 23:24:47 -0500

    end day

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
-#include "gtk_auto.h"
+// #include "gtk_auto.h"
 #include <gtk/gtk.h>
 
 // funtion construct
 static void activate (GtkApplication *app, gpointer user_data) {
 
     GtkWidget *window, *mainBox;
-    GtkWidget *buttboxConvert, *buttboxHam, *buttBoxError, * buttBoxSuma;
+    GtkWidget *buttBoxConvert, *buttBoxHam, *buttBoxError, * buttBoxSuma;
     GtkWidget *buttonConvert, *buttonHam, *buttonError, *buttonSuma;
+    GtkStyleContext *style;
+
+GtkWindow GtkButton->background-color = #898989;
+    // Set Style for buttons
+    style_set(buttonConvert, style);
+
+    // Create mian container box 
+    mainBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
 
     // Create box for buttons
-    buttBoxConvert = gtk_button_box_new();
-    buttBoxHam = gtk_button_box_new();
-    buttBoxError = gtk_button_box_new();
-    buttBoxSuma = gtk_button_box_new();
+    buttBoxConvert = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    buttBoxHam = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    buttBoxError = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    buttBoxSuma = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
 
     // Create buttons and add at button box
     buttonConvert = gtk_button_new_with_label("Binario - Gray");
@@ -20,7 +28,23 @@ static void activate (GtkApplication *app, gpointer user_data) {
     buttonHam = gtk_button_new_with_label("Código Hamming");
     buttonError = gtk_button_new_with_label("Deteccion y correcion de errores");
 
-    mainBox = gtk_box_new();
+    gtk_button_set_relief(GTK_BUTTON(buttonConvert), GTK_RELIEF_HALF);
+
+    gtk_container_add(GTK_CONTAINER(buttBoxConvert), buttonConvert);
+    gtk_container_add(GTK_CONTAINER(buttBoxHam), buttonHam);
+    gtk_container_add(GTK_CONTAINER(buttBoxError), buttonError);
+    gtk_container_add(GTK_CONTAINER(buttBoxSuma), buttonSuma);
+
+    gtk_widget_set_size_request(GTK_WIDGET(buttonConvert), 240, 40);
+    gtk_widget_set_size_request(GTK_WIDGET(buttonSuma), 240, 40);
+    gtk_widget_set_size_request(GTK_WIDGET(buttonError), 240, 40);
+    gtk_widget_set_size_request(GTK_WIDGET(buttonHam), 240, 40);
+
+    // add box buttons at main box
+    gtk_box_pack_start(GTK_BOX(mainBox), buttBoxConvert, TRUE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(mainBox), buttBoxHam, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(mainBox), buttBoxSuma, TRUE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(mainBox), buttBoxError, TRUE, TRUE, 10);
         
     // Set properties for winow
     window = gtk_application_window_new (app);
@@ -30,7 +54,7 @@ static void activate (GtkApplication *app, gpointer user_data) {
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
     
-    gtk_container_add(GTK_CONTAINER(window), fixed);
+    gtk_container_add(GTK_CONTAINER(window), mainBox);
     gtk_widget_show_all (window);
 }
 
@@ -46,4 +70,4 @@ int main (int argc, char **argv) {
     return status;
 }
 
-// Compiler gcc `pkg-config --cflags gtk+-3.0` -o hannoi hannoi.c `pkg-config --libs gtk+-3.0`
+// Compiler gcc `pkg-config --cflags gtk+-3.0` -o main main.c `pkg-config --libs gtk+-3.0`
